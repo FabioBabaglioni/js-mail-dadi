df4670774e9d114438c28dc6e43024a74cd155ae
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-10 15:46:42 +0100

    mail delìgli invitati stampate

--- Esercizio mail/js/main.js
+++ Esercizio mail/js/main.js
@@ -2,6 +2,12 @@
 
 
 // crea array con tutte le mail degli invitati
+const invitati = ["pino@gmail.com", "giovannirossi@gmail.com", "lorenzocrett@gmail.com",  "matteouccio@gmail.com", "chiarapl@gmail.com", "babafab@gmail.com", "luciasanta@gmail.com", "pinopinguino@libero.com", "nicolaluce@libero.com", "torioup@server.com",]
+
+for(let i = 0; i<invitati.length; i++){
+    console.log(invitati [i])
+}
+
 
 
 // chiedi all'utente la sua mail
