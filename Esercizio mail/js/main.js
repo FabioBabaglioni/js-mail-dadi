9f6bb0b87d8f725cf40e830d69c021a291e44637
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-10 15:36:31 +0100

    Creazione file e commenti generici

---
+++ Esercizio mail/js/main.js
@@ -0,0 +1,13 @@
+// capire se l'utente ha l'invito
+
+
+// crea array con tutte le mail degli invitati
+
+
+// chiedi all'utente la sua mail
+
+
+// se la mail è nella lista allora di che può partecipare
+
+
+// se la mail non è nella lista non può partecipare
